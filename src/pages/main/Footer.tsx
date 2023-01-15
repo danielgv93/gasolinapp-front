737b684ac4305ff127c6c6de7d0b5acb59ac518f
Dani <lequodo@gmail.com>
2023-01-15 03:42:23 +0100

    Add footer

---
+++ src/pages/main/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer
+            className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                    2023. Made with ❤️ by <a href="https://github.com/danielgv93" className="hover:underline">Daniel Garcia Varela</a></span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
