e90559a72026ee75992438ab9461f72d581ab03f
Daniel Garcia <lequodo@gmail.com>
2023-07-22 13:26:10 +0200

    Update constants.ts

--- src/domain/core/constants.ts
+++ src/domain/core/constants.ts
@@ -1,6 +1,6 @@
 export const Api = {
     baseUrl: (process.env.NODE_ENV === 'development')
         ? process.env.REACT_APP_API_URL
-        : 'https://gasolinapp.onrender.com',
+        : 'https://gasolinapp.chorizox.app:5000',
 
 }
